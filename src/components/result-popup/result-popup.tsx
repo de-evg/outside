1536a8431b73d04dfdc3b5ff16c4e97d84ab2be0
de-evg <d.e.min@yandex.ru>
2021-03-19 20:33:21 +0300

    done

--- src/components/result-popup/result-popup.tsx
+++ src/components/result-popup/result-popup.tsx
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 import { NameSpace } from "../../store/reducers/root";
 
 const DEFAULT_YEARS: string[] = [];
-const Decrease: { PAYMENT: string, TIME: string } = {
+const Decrease: { [x: string]: string } = {
   PAYMENT: "Сокращения платежа",
   TIME: "Сокращение срока"
 };
@@ -54,33 +54,64 @@ const Title = styled.h1`
 `;
 
 const Table = styled.table`
+  margin-bottom: 15px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TableTitle = styled.caption`
+  text-align: left;
+  color: #000000;  
+  font-family: "LabGrotesque", "Arial", sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 500;
+`;
+
+const TableBody = styled.tbody`
   width: 100%;
   display: flex;
   flex-direction: column;
 `;
 
 const TableRow = styled.tr`
-  width: 100%;
+  padding: 10px 0;
   display: flex;
+  border-bottom: 1px solid #DFE3E6;
 `;
 
 const TableHead = styled.th`
-  
+  text-align: left;
+  flex-basis: 70%;
+  width: 70%;
+  font-family: "LabGrotesque", "Arial", sans-serif;
+  font-size: 14px;
+  line-height: 24px;
+  font-weight: 500;
 `;
 
 const TableData = styled.td`
-  flex-grow: 1;
+  flex-basis: 30%;
+  width: 30%;
+  flex-wrap: nowrap;
   text-align: right;
-  align-self: center;
+  font-family: "LabGrotesque", "Arial", sans-serif;
+  font-size: 14px;
+  line-height: 24px;
+  font-weight: 400;
 `;
 
 interface IResultProps {
   salary: number;
+  taxOfSalary: number;
+  costOfRealty: number;
+  taxOfRealty: number;
   paymentByYears: { [x: string]: number };
   decreaseBy: string;
 };
 
-const ResultPopup: React.FC<IResultProps> = ({ salary, paymentByYears, decreaseBy }: IResultProps) => {
+const ResultPopup: React.FC<IResultProps> = ({ salary, taxOfSalary, costOfRealty, taxOfRealty, paymentByYears, decreaseBy }: IResultProps) => {
   const [years, setYears] = React.useState<string[]>(DEFAULT_YEARS);
 
   React.useEffect(() => {
@@ -95,21 +126,38 @@ const ResultPopup: React.FC<IResultProps> = ({ salary, paymentByYears, decreaseB
       <CloseButton />
       <Title>Результат</Title>
       <Table>
-        <TableRow>
-          <TableHead>Зарплата</TableHead>
-          <TableData>{salary}</TableData>
-        </TableRow>
-        <TableRow>
-          <TableHead>Тип операции</TableHead>
-          <TableData>{Decrease[decreaseBy.toUpperCase()]}</TableData>
-        </TableRow>
-        <TableRow>
-          <TableHead>Налоговые вычеты</TableHead>
-        </TableRow>
+        <TableTitle>Сводные данные</TableTitle>
+        <TableBody>
+          <TableRow>
+            <TableHead>Стоимость недвижимости</TableHead>
+            <TableData>{costOfRealty} ₽</TableData>
+          </TableRow>
+          <TableRow>
+            <TableHead>Налоговый вычет с недвижимости</TableHead>
+            <TableData>{taxOfRealty} ₽</TableData>
+          </TableRow>
+          <TableRow>
+            <TableHead>Годовая зарплата</TableHead>
+            <TableData>{salary * 12} ₽</TableData>
+          </TableRow>
+          <TableRow>
+            <TableHead>Налоговый вычет с ЗП за год</TableHead>
+            <TableData>{taxOfSalary} ₽</TableData>
+          </TableRow>
+          <TableRow>
+            <TableHead>Тип операции</TableHead>
+            <TableData>{Decrease[decreaseBy.toUpperCase()]}</TableData>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <Table>
+        <TableTitle>Налоговые вычеты</TableTitle>
+        <TableBody>
+        </TableBody>
         {
-          years.map((year) => <TableRow>
+          years.map((year, i) => <TableRow key={`${i}-${year}`}>
             <TableHead>{year.replace("year-", "Год ")}</TableHead>
-            <TableData>{paymentByYears[year]} рублей</TableData>
+            <TableData>{paymentByYears[year]} ₽</TableData>
           </TableRow>)
         }
       </Table>
@@ -120,6 +168,9 @@ const ResultPopup: React.FC<IResultProps> = ({ salary, paymentByYears, decreaseB
 interface IResultState {
   [x: string]: {
     salary: number;
+    taxOfSalary: number;
+    costOfRealty: number;
+    taxOfRealty: number;
     paymentByYears: { [x: string]: number };
     decreaseBy: string;
   }
@@ -127,6 +178,9 @@ interface IResultState {
 
 const mapStateToProps = (state: IResultState) => ({
   salary: state[NameSpace.NUMBERS].salary,
+  taxOfSalary: state[NameSpace.NUMBERS].taxOfSalary,
+  costOfRealty: state[NameSpace.NUMBERS].costOfRealty,
+  taxOfRealty: state[NameSpace.NUMBERS].taxOfRealty,
   paymentByYears: state[NameSpace.NUMBERS].paymentByYears,
   decreaseBy: state[NameSpace.INTERFACE].decreaseBy,
 });
